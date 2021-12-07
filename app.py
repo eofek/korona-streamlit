1c41451756632c7068f87610e524553f6b6ba4e0
eof <eof@MacBook-Air-eof.local>
2021-12-07 13:37:50 +0100

    first attempt to print data from mongodb

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ st.title("wszyscy umrzemy")
 def get_data():
     lnk = pymongo.MongoClient("mongodb+srv://streamlit:$Upv.AF63u-WARG@covidcases.uh3sr.mongodb.net/covidCasesPL?retryWrites=true&w=majority")
     client = pymongo.MongoClient(lnk)
-    db = client.covidCasesPL
-    collection = db.PL
+    collection = client['covidCasesPL']['PL']
     df = pd.DataFrame(list(collection.find({})))
 
     return df
