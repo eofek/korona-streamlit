b70c6dfa0ddba4b52f55ca6b1202c51b12f0c0a5
eof <eof@MacBook-Air-eof.local>
2021-12-07 13:01:59 +0100

    first attempt to print data from mongodb

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import pymongo
 
 st.title("wszyscy umrzemy")
 
@@ -8,3 +8,16 @@ st.title("wszyscy umrzemy")
 #
 # st.write(df)
 
+# Initialize connection.
+client = pymongo.MongoClient(**st.secrets["mongo"])
+
+@st.cache(ttl=600)
+def get_data():
+    db = client.mydb
+    items = db.mycollection.find()
+    items = list(items)  # make hashable for st.cache
+    return items
+
+items = get_data()
+
+st.write(items)
