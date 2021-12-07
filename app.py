e290f68f54f26db7e963429d0743e346415a71f4
eof <eof@MacBook-Air-eof.local>
2021-12-07 13:09:13 +0100

    first attempt to print data from mongodb

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ client = pymongo.MongoClient(**st.secrets["mongo"])
 
 @st.cache(ttl=600)
 def get_data():
-    db = client.covidCasesPL
+    db = client['covidCasesPL']['PL']
     items = db.mycollection.find()
     items = list(items)  # make hashable for st.cache
     return items
