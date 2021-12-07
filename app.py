a56bcfbcb8fde616187c7c789d3bb2a6d800138f
eof <eof@MacBook-Air-eof.local>
2021-12-07 15:29:25 +0100

    works

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 from pymongo import MongoClient
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+import matplotlib.ticker as ticker
 
 st.title("wszyscy umrzemy")
 
@@ -19,6 +25,24 @@ def get_data():
 
     return df
 
-items = get_data()
+cases = get_data()
 
-st.write(items)
+cases.sort_values(by='date',  ascending=True, inplace=True)
+
+cases['cases'] = cases.cases.astype(int)
+
+palette= sns.color_palette('YlOrRd', cases.cases.count())
+fig, axe = plt.subplots(figsize=(16,9))
+axe.tick_params(axis='x', rotation=55, labelsize=18)
+axe.tick_params(axis='y', rotation=55, labelsize=18)
+axe.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), '_')))
+sns.barplot(x=pd.to_datetime(cases.date).dt.strftime("%d-%b"), y=cases.cases,palette=palette);
+sns.lineplot(x=pd.to_datetime(cases.date).dt.strftime("%d-%b"), y=cases.cases.rolling(7).mean(), color='r', linewidth=4)
+axe.xaxis.set_major_locator(ticker.MultipleLocator(10))
+
+st.pyplot(fig)
+
+cases.sort_values(by='date',  ascending=False, inplace=True)
+
+
+st.table(cases.reset_index(drop=True))
